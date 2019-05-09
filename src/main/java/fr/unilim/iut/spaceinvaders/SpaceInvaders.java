9f5dd2293603ceb118a7502abd8814696ec3d2f3
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2019-05-09 08:29:04 +0200

    fin de "impossible positionner vaisseau qui déborde de l'espace de jeu"

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,5 +1,6 @@
 package fr.unilim.iut.spaceinvaders;
 
+import utils.DebordementEspaceJeuException;
 import utils.HorsEspaceJeuException;
 
 public class SpaceInvaders {
@@ -62,6 +63,12 @@ public class SpaceInvaders {
 	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
 		if (!estDansEspaceJeu(x, y))
 			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		if (!estDansEspaceJeu(x + longueur - 1, y))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers la droite à cause de sa longueur");
+		
+		if (!estDansEspaceJeu(x, y - hauteur + 1))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers le bas à cause de sa hauteur");
 
 		vaisseau = new Vaisseau(longueur, hauteur);
 		vaisseau.positionner(x, y);
